6dd2f1cc1c4188abfc7ecd8c3da270d70f9a6ee7
Zicklag <zicklag@katharostech.com>
2025-04-25 16:23:24 -0500

    feat: templatize a lot of the components and configure with a YAMl file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import ViteYaml from '@modyfi/vite-plugin-yaml';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [tailwind(), icon()],
+  vite: {
+    plugins: [ViteYaml()]
+  }
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,37 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="@modyfi/vite-plugin-yaml/modules" />
+
+import type { string } from "astro:schema";
+import type { CompatibilityItem } from "./types";
+
+declare module "~/config.yaml" {
+  import type { FeatureItem } from "./types";
+  type Section = { id: string; title: string; lead: string } & (
+    | {
+        type: "content";
+        icon?: string;
+        links?: { label: string; url: string; icon?: string }[];
+      }
+    | {
+        type: "compatibility";
+        lists: {
+          title: string;
+          items: CompatibilityItem[];
+          url?: string;
+        }[];
+      }
+    | {
+        type: "features";
+        title?: string;
+        features: FeatureItem[];
+      }
+  );
+  const value: {
+    title: string;
+    description: string;
+    heroText: string;
+    sections: Section[];
+  };
+  export default value;
+}
